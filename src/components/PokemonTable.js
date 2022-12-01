55b21a53793953663af1ffa9702fab7b08ad09b4
CalielTW <mikywaay@hotmail.com>
2022-11-30 18:58:54 -0600

    c3

---
+++ src/components/PokemonTable.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+export default function BasicTable({pokemons}) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Pokemon</TableCell>
+            <TableCell align="right">url</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {pokemons.map((pokemon) => (
+            <TableRow
+              key={pokemon.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}  >
+              <TableCell component="th" scope="row"> {pokemon.name} </TableCell>
+              <TableCell align="right">{pokemon.url}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
